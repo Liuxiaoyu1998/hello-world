9df2f13b811fda921f909c38391dd2a4ce2ed231
banye1998 <37111444+Liuxiaoyu1998@users.noreply.github.com>
2018-06-24 14:15:18 +0800

    update units
    ver 1

--- 打表.cpp
+++ 打表.cpp
@@ -1,83 +1,84 @@
-double units[5][100];
-//¹¦ÂÊ power
-/* 
-1	ÍßÌØ(w)
-1	½¹¶ú/Ãë(J/s)
-1	Å£¶Ù¡¤Ã×/Ãë(N¡¤m/s)
-0.0013596	Ã×ÖÆÂíÁ¦(ps)
-0.000239	Ç§¿¨/Ãë(kcal/s)
-0.001	Ç§Íß(kW)
-0.001341	Ó¢ÖÆÂíÁ¦(hp)
-0.7375621	Ó¢³ß¡¤°õ/Ãë(ft¡¤lb/s)
-0.0009478	Ó¢ÈÈµ¥Î»/Ãë(Btu/s)
-0.1019716	¹«½ï¡¤Ã×/Ãë(kg¡¤m/s)
-*/ 
-units[0] = {1, 1, 1, 0.0013596, 0.000239, 0.001, 0.001341, 0.7375621, 0.0009478, 0.1019716};
-
-//Ñ¹Ç¿ pressure 
+//æ¸©åº¦ temperature
 /*
-1		ÅÁ(pa) 
-0.01	°ÙÅÁ(hpa)
-0.01	ºÁ°Í(mbar)
-0.001	Ç§ÅÁ(kpa)
-0.00001	°Í(bar)
-1e-6	Õ×ÅÁ(MPa)
-0.0075006	ºÁÃ×¹¯Öù(mmHg)
-0.101972	ºÁÃ×Ë®Öù
-0.0002953	Ó¢´ç¹¯Öù(in Hg)
-0.000145	°õÁ¦/Æ½·½Ó¢´ç(psi)
-9.8692e-6	±ê×¼´óÆøÑ¹(atm)
-0.0208854	°õÁ¦/Æ½·½Ó¢³ß(psf)
-0.0000102	¹«½ïÁ¦/Æ½·½ÀåÃ×(kgf/cm2)
-0.1019716	¹«½ïÁ¦/Æ½·½Ã×(kgf/©O)
-*/
-units[1] = {1, 0.01, 0.01, 0.001, 0.00001, 0.000001, 0.0075006, 0.101972, 
-			0.0002953, 0.000145, 9.8692e-6, 0.0208854, 0.0000102, 0.1019716};
- 
-//ÎÂ¶È temperature
-/*
-1		ÉãÊÏ¶È(¡æ) 
-33.8	»ªÊÏ¶È(¨H)
-274.15	¿ªÊÏ¶È(K)
-0.8	ÁĞÊÏ¶È(¡ãRe)
-493.47	À¼ÊÏ¶È(¡ãR)
+1		æ‘„æ°åº¦(â„ƒ) 
+33.8	åæ°åº¦(â„‰)
+274.15	å¼€æ°åº¦(K)
+0.8	åˆ—æ°åº¦(Â°Re)
+493.47	å…°æ°åº¦(Â°R)
 */
 units[2] = {1, 33.8, 274.15, 0.8, 493.47};
 
-//Ìå»ı volume
-/*
-1		Á¢·½Ã×(m3)
-1000	Éı(l)
-1000000	ºÁÉı(ml)
-1000000	Á¢·½ÀåÃ×(cm3)
-1000	Á¢·½·ÖÃ×(dm3)
-10000	·ÖÉı(dl)
-100000	ÀåÉı(cl)
-10	¹«Ê¯(hl)
-1000000000	Î¢Éı(ul)
-1000000000	Á¢·½ºÁÃ×(mm3)
-61023.8445022	Á¢·½Ó¢´ç(cu in)
-1.3079528	Á¢·½Âë(cu yd)
-35.3147248	Á¢·½Ó¢³ß(cu ft)
-0.0008107	Ä¶Ó¢³ß
-219.9691573	Ó¢ÖÆ¼ÓÂØ(uk gal)
-264.1720524	ÃÀÖÆ¼ÓÂØ(us gal)
-35198.873636	Ó¢ÖÆÒºÌå°»Ë¾(oz)
-34164.6737274	ÃÀÖÆÒºÌå°»Ë¾(oz)
-*/
-units[3] = {1, 1000, 1000000, 1000000, 1000, 10000, 100000, 10, 1000000000,
-			1000000000, 61023.8445022, 1.3079528, 35.3147248, 0.0008107,
-			219.9691573, 264.1720524, 35198.873636, 34164.6737274};
+vector<string> temperature_name;
+vector<double> temperature_mul;
+//ç¬¦å·
+temperature_name.push_back("æ‘„æ°åº¦(â„ƒ)"); temperature_name.push_back("åæ°åº¦(â„‰)"); temperature_name.push_back("å¼€æ°åº¦(K)");
+temperature_name.push_back("åˆ—æ°åº¦(Â°Re)"); temperature_name.push_back("å…°æ°åº¦(Â°R)");
 
-//ËÙ¶È velocity
-/*
-1	Ã×/Ãë(m/s)
-3.6	Ç§Ã×/Ê±(km/h)
-0.001	Ç§Ã×/Ãë(km/s)
-0.0029386	ÂíºÕ(mach)
-39.370079	Ó¢´ç/Ãë(in/s)
-3.3356e-9	¹âËÙ(c)
-2.236936	Ó¢Àï/Ê±(mile/h)
-*/
-units[4] = {1, 3.6, 0.001, 0.0029386, 39.370079, 0.0000000033356, 2.236936};
+temperature_mul.pushback(1); temperature_mul.pushback(33.8); temperature_mul.pushback(274.15);
+temperature_mul.pushback(0.8); temperature_mul.pushback(493.47);
 
+
+//åŠŸç‡ power
+units[0] = {1, 1, 1, 0.0013596, 0.000239, 0.001, 0.001341, 0.7375621, 0.0009478, 0.1019716};
+
+vector<string> power_name;
+vector<double> power_mul;
+power_name.push_back("w"); power_name.push_back("J/s"); power_name.push_back("N.m/s"); 
+power_name.push_back("ps"); power_name.push_back("kcal/s"); power_name.push_back("kw"); 
+power_name.push_back("hp"); power_name.push_back("ftÂ·lb/s"); power_name.push_back("Btu/s"); 
+power_name.push_back("kg.m/s");
+
+power_mul.push_back(1); power_mul.push_back(1); power_mul.push_back(1); 
+power_mul.push_back(0.0013596); power_mul.push_back(0.000239); power_mul.push_back(0.001); 
+power_mul.push_back(0.001341); power_mul.push_back(0.7375621); power_mul.push_back(0.0009478); 
+power_mul.push_back(0.1019716); 
+
+//å‹å¼º pressure 
+units[1] = {1, 0.01, 0.01, 0.001, 0.00001, 0.000001, 0.0075006, 0.0002953,
+			 0.000145, 0.0000098692, 0.0208854, 0.0000102, 0.1019716};
+			
+vector<string> pressure_name;
+vector<double> pressure_mul;
+
+pressure_name.push_back("pa"); pressure_name.push_back("hpa"); pressure_name.push_back("mbar"); 
+pressure_name.push_back("kpa"); pressure_name.push_back("bar"); pressure_name.push_back("MPa"); 
+pressure_name.push_back("mmHg"); pressure_name.push_back("inHg"); pressure_name.push_back("psi"); 
+pressure_name.push_back("atm"); pressure_name.push_back("psf"); pressure_name.push_back("kgf/cm2"); 
+pressure_name.push_back("kgf/m2"); 
+
+pressure_mul.push_back(1); pressure_mul.push_back(0.01); pressure_mul.push_back(0.01); 
+pressure_mul.push_back(0.001); pressure_mul.push_back(0.00001); 
+pressure_mul.push_back(0.000001); pressure_mul.push_back(0.0075006); 
+pressure_mul.push_back(0.0002953); pressure_mul.push_back(0.000145); pressure_mul.push_back(0.0000098692); 
+pressure_mul.push_back(0.0208854); pressure_mul.push_back(0.0000102); pressure_mul.push_back(0.1019716); 
+
+
+//ä½“ç§¯ volume
+vector<string> volume_name;
+vector<double> volume_mul;
+
+volume_name.push_back("m3"); volume_name.push_back("l"); volume_name.push_back("ml"); 
+volume_name.push_back("cm3"); volume_name.push_back("dm3"); volume_name.push_back("dl"); 
+volume_name.push_back("cl"); volume_name.push_back("hl"); volume_name.push_back("ul"); 
+volume_name.push_back("mm3"); volume_name.push_back("cu_in"); volume_name.push_back("cu_yd"); 
+volume_name.push_back("cu_ft"); volume_name.push_back("uk_gal"); volume_name.push_back("us_gal"); 
+volume_name.push_back("uk_loz"); volume_name.push_back("us_loz"); 
+
+volume_mul.push_back(1); volume_mul.push_back(1000); volume_mul.push_back(1000000); 
+volume_mul.push_back(1000000); volume_mul.push_back(1000); volume_mul.push_back(10000); 
+volume_mul.push_back(100000); volume_mul.push_back(10); volume_mul.push_back(1000000000); 
+volume_mul.push_back(1000000000); volume_mul.push_back(61023.8445022); volume_mul.push_back(1.3079528); 
+volume_mul.push_back(35.3147248); volume_mul.push_back(219.9691573); volume_mul.push_back(264.1720524); 
+volume_mul.push_back(35198.873636); volume_mul.push_back(34164.6737274); 
+
+//é€Ÿåº¦ velocity
+vector<string> velocity_name;
+vector<double> velocity_mul;
+
+velocity_name.push_back("m/s"); velocity_name.push_back("km/h"); velocity_name.push_back("km/s");
+velocity_name.push_back("mach"); velocity_name.push_back("in/s"); velocity_name.push_back("c"); 
+velocity_name.push_back("mile/h"); 
+
+velocity_mul.push_back(1); velocity_mul.push_back(3.6); velocity_mul.push_back(0.001); 
+velocity_mul.push_back(0.0029386); velocity_mul.push_back(39.370079); velocity_mul.push_back(0.0000000033356); 
+velocity_mul.push_back(2.236936); 
